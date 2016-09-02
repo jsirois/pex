ff10dd89eab6491becfc849387261a0fecb1d68c
John Sirois <john.sirois@gmail.com>
2016-09-02 15:27:11 -0600

    1st cut at safe extraction of UTF-8 archive filenames.

--- pex/archiver.py
+++ pex/archiver.py
@@ -1,12 +1,15 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import codecs
 import contextlib
+import functools
 import os
 import tarfile
 import zipfile
 
-from .common import safe_mkdtemp
+from .common import safe_mkdir, safe_mkdtemp
+from .compatibility import to_bytes
 
 
 class Archiver(object):
@@ -14,12 +17,52 @@ class Archiver(object):
   class UnpackError(Error): pass
   class InvalidArchive(Error): pass
 
+  _TAR_EXTRACTOR = (functools.partial(tarfile.open, encoding='utf-8'), tarfile.ReadError)
+
+  class ZipExtractor(object):
+    """Allows extraction of zip files with UTF-8 file names.
+
+    Some unix systems (Linux) may have non UTF-8 default character encodings in which case using
+    `ZipFile.extractall` directly can fail.
+
+    See:
+      https://github.com/pantsbuild/pex/issues/298
+      https://github.com/pantsbuild/pants/issues/3823
+    """
+
+    _UTF_8 = codecs.lookup('utf-8')
+
+    def __init__(self, filename):
+      self._zipfile = zipfile.ZipFile(filename)
+
+    def extractall(self, path=None):
+      extract_dir = to_bytes(os.path.realpath(path or os.curdir))
+      for info in self._zipfile.infolist():
+        filename, _ = self._UTF_8.encode(info.filename)
+        if os.path.isabs(filename):
+          raise Archiver.UnpackError(
+              'Refusing to unpack archive member with absolute path: {}'.format(filename))
+        if filename.endswith(b'/'):
+          abs_dir = os.path.join(extract_dir, filename)
+          safe_mkdir(abs_dir)
+        else:
+          rel_dir = os.path.dirname(filename)
+          abs_dir = os.path.join(extract_dir, rel_dir)
+          safe_mkdir(abs_dir)
+
+          abs_path = os.path.join(abs_dir, os.path.basename(filename))
+          with open(abs_path, 'wb') as fp:
+            fp.write(self._zipfile.read(info))
+
+    def close(self):
+      self._zipfile.close()
+
   EXTENSIONS = {
-    '.tar': (tarfile.TarFile.open, tarfile.ReadError),
-    '.tar.gz': (tarfile.TarFile.open, tarfile.ReadError),
-    '.tar.bz2': (tarfile.TarFile.open, tarfile.ReadError),
-    '.tgz': (tarfile.TarFile.open, tarfile.ReadError),
-    '.zip': (zipfile.ZipFile, zipfile.BadZipfile)
+    '.tar': _TAR_EXTRACTOR,
+    '.tar.gz': _TAR_EXTRACTOR,
+    '.tar.bz2': _TAR_EXTRACTOR,
+    '.tgz': _TAR_EXTRACTOR,
+    '.zip': (ZipExtractor, zipfile.BadZipfile)
   }
 
   @classmethod
