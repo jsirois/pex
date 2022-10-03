db653ab18958d6ff1de0ce15fa8aa14611041a66
John Sirois <john.sirois@gmail.com>
2022-10-02 20:06:01 -0700

    Add updated ziputils and test.

--- pex/ziputils.py
+++ pex/ziputils.py
@@ -7,7 +7,6 @@ import io
 import os
 import shutil
 import struct
-from typing import Tuple
 
 from pex.typing import TYPE_CHECKING
 
@@ -19,94 +18,49 @@ else:
     from pex.third_party import attr
 
 
+class ZipError(Exception):
+    """Indicates a problem reading a zip file."""
+
+
 @attr.s(frozen=True)
-class _FileHeaderRecord(object):
-    _STRUCT = struct.Struct("<4sHHHHHHLLLHHHHHLL")
+class _Zip64Error(ZipError):
+    """Indicates Zip64 support is required but not implemented."""
 
-    _SIGNATURE = b"\x50\x4b\x01\x02"
+    record_type = attr.ib()  # type: str
+    field = attr.ib()  # type: str
+    value = attr.ib()  # type: int
+    message = attr.ib(default="")  # type: str
 
-    @classmethod
-    def load(cls, fp):
-        # type: (BinaryIO) -> Optional[_FileHeaderRecord]
-        maybe_sig = fp.read(len(cls._SIGNATURE))
-        if cls._SIGNATURE != maybe_sig:
-            return None
-
-        fp.seek(-len(cls._SIGNATURE), os.SEEK_CUR)
-        file_header_record = cls(*cls._STRUCT.unpack(fp.read(cls._STRUCT.size)))
-        return attr.evolve(
-            file_header_record,
-            file_name=fp.read(file_header_record.file_name_length),
-            extra_field=fp.read(file_header_record.extra_field_length),
-            file_comment=fp.read(file_header_record.file_comment_length),
+    def __str__(self):
+        # type: () -> str
+        message_lines = [self.message] if self.message else []
+        message_lines.append(
+            "The {field} field of the {record_type} record has value {value} indicating Zip64 "
+            "support is required, but Zip64 support is not implemented.".format(
+                record_type=self.record_type,
+                field=self.field,
+                value=self.value,
+            )
         )
+        message_lines.append(
+            "Please file an issue at https://github.com/pantsbuild/pex/issues/new that includes "
+            "this full backtrace if you need this support."
+        )
+        return os.linesep.join(message_lines)
 
-    # See: https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
-    # 4.3.12  Central directory structure:
-    #
-    #    [central directory header 1]
-    #    .
-    #    .
-    #    .
-    #    [central directory header n]
-    #    [digital signature]
-    #
-    #    File header:
-    #
-    #         central file header signature   4 bytes  (0x02014b50)
-    #         version made by                 2 bytes
-    #         version needed to extract       2 bytes
-    #         general purpose bit flag        2 bytes
-    #         compression method              2 bytes
-    #         last mod file time              2 bytes
-    #         last mod file date              2 bytes
-    #         crc-32                          4 bytes
-    #         compressed size                 4 bytes
-    #         uncompressed size               4 bytes
-    #         file name length                2 bytes
-    #         extra field length              2 bytes
-    #         file comment length             2 bytes
-    #         disk number start               2 bytes
-    #         internal file attributes        2 bytes
-    #         external file attributes        4 bytes
-    #         relative offset of local header 4 bytes
-    #
-    #         file name (variable size)
-    #         extra field (variable size)
-    #         file comment (variable size)
 
-    sig = attr.ib()  # type: bytes
-    version_made_by = attr.ib()  # type: int
-    version_needed_to_extract = attr.ib()  # type: int
-    general_purpose_bit_flag = attr.ib()  # type: int
-    compression_method = attr.ib()  # type: int
-    last_mod_file_time = attr.ib()  # type: int
-    last_mod_file_date = attr.ib()  # type: int
-    crc32 = attr.ib()  # type: int
-    compressed_size = attr.ib()  # type: int
-    uncompressed_size = attr.ib()  # type: int
-    file_name_length = attr.ib()  # type: int
-    extra_field_length = attr.ib()  # type: int
-    file_comment_length = attr.ib()  # type: int
-    disk_number_start = attr.ib()  # type: int
-    internal_file_attributes = attr.ib()  # type: int
-    external_file_attributes = attr.ib()  # type: int
-    relative_offset_of_local_header = attr.ib()  # type: int
-
-    file_name = attr.ib(default=b"")  # type: bytes
-    extra_field = attr.ib(default=b"")  # type: bytes
-    file_comment = attr.ib(default=b"")  # type: bytes
+_MAX_2_BYTES = 0xFFFF
+_MAX_4_BYTES = 0xFFFFFFFF
 
 
 @attr.s(frozen=True)
 class _EndOfCentralDirectoryRecord(object):
+    _SIGNATURE = b"\x50\x4b\x05\x06"
     _STRUCT = struct.Struct("<4sHHHHLLH")
 
-    _SIGNATURE = b"\x50\x4b\x05\x06"
     _MAX_SIZE = _STRUCT.size + (
         # The comment field is of variable length but that length is capped at a 2 byte integer.
-        2
-        ^ 16
+        _MAX_2_BYTES
     )
 
     @classmethod
@@ -123,26 +77,18 @@ class _EndOfCentralDirectoryRecord(object):
             fp.seek(-cls._STRUCT.size, os.SEEK_END)
             if cls._SIGNATURE == fp.read(len(cls._SIGNATURE)):
                 fp.seek(-len(cls._SIGNATURE), os.SEEK_CUR)
-                eocd_record = cls(cls._STRUCT.size, *cls._STRUCT.unpack(fp.read()))
-            else:
-                # There must be an EOCD comment, rewind to allow for the biggest possible comment (
-                # which is not that big at all).
-                read_size = min(cls._MAX_SIZE, file_size)
-                fp.seek(-read_size, os.SEEK_END)
-                last_data_chunk = fp.read()
-                start_eocd = last_data_chunk.find(cls._SIGNATURE)
-                _struct = cls._STRUCT.unpack_from(last_data_chunk, start_eocd)
-                zip_comment = last_data_chunk[start_eocd + cls._STRUCT.size :]
-                eocd_record = cls(len(last_data_chunk) - start_eocd, *(_struct + (zip_comment,)))
+                return cls(cls._STRUCT.size, *cls._STRUCT.unpack(fp.read()))
 
-            fp.seek(-(eocd_record.cd_size + eocd_record.size), os.SEEK_END)
-            file_header_records = []
-            while True:
-                file_header_record = _FileHeaderRecord.load(fp)
-                if file_header_record is None:
-                    break
-                file_header_records.append(file_header_record)
-            return attr.evolve(eocd_record, file_header_records=tuple(file_header_records))
+            # There must be an EOCD comment, rewind to allow for the biggest possible comment (
+            # which is not that big at all).
+            read_size = min(cls._MAX_SIZE, file_size)
+            fp.seek(-read_size, os.SEEK_END)
+            last_data_chunk = fp.read()
+            start_eocd = last_data_chunk.find(cls._SIGNATURE)
+            _struct = cls._STRUCT.unpack_from(last_data_chunk, start_eocd)
+            zip_comment = last_data_chunk[start_eocd + cls._STRUCT.size :]
+            eocd_size = len(last_data_chunk) - start_eocd
+            return cls(eocd_size, *(_struct + (zip_comment,)))
 
     size = attr.ib()  # type: int
 
@@ -165,50 +111,78 @@ class _EndOfCentralDirectoryRecord(object):
     #       .ZIP file comment       (variable size)
 
     sig = attr.ib()  # type: bytes
-    disk_no = attr.ib()  # type: int
-    cd_disk_no = attr.ib()  # type: int
-    disk_cd_record_count = attr.ib()  # type: int
-    total_cd_record_count = attr.ib()  # type: int
-    cd_size = attr.ib()  # type: int
-    cd_offset = attr.ib()  # type: int
+    disk_no = attr.ib(metadata={"max": _MAX_2_BYTES})  # type: int
+    cd_disk_no = attr.ib(metadata={"max": _MAX_2_BYTES})  # type: int
+    disk_cd_record_count = attr.ib(metadata={"max": _MAX_2_BYTES})  # type: int
+    total_cd_record_count = attr.ib(metadata={"max": _MAX_2_BYTES})  # type: int
+    cd_size = attr.ib(metadata={"max": _MAX_4_BYTES})  # type: int
+    cd_offset = attr.ib(metadata={"max": _MAX_4_BYTES})  # type: int
     zip_comment_size = attr.ib()  # type: int
     zip_comment = attr.ib(default=b"")  # type: bytes
-    file_header_records = attr.ib(default=())  # type: Tuple[_FileHeaderRecord, ...]
+
+    @disk_no.validator
+    @cd_disk_no.validator
+    @disk_cd_record_count.validator
+    @total_cd_record_count.validator
+    @cd_size.validator
+    @cd_offset.validator
+    def _validate_does_not_require_zip64(
+        self,
+        attribute,  # type: attr.Attribute
+        value,  # type: int
+    ):
+        # See: https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
+        #
+        # 4.4.1.4  If one of the fields in the end of central directory
+        #       record is too small to hold required data, the field SHOULD be
+        #       set to -1 (0xFFFF or 0xFFFFFFFF) and the ZIP64 format record
+        #       SHOULD be created.
+        if value == attribute.metadata["max"]:
+            raise _Zip64Error(
+                record_type="EndOfCentralDirectoryRecord", field=attribute.name, value=value
+            )
+
+    @property
+    def start_of_zip_offset_from_eof(self):
+        # type: () -> int
+        return self.size + self.cd_size + self.cd_offset
 
 
 @attr.s(frozen=True)
 class Zip(object):
+    """Allows interacting with a Zip that may have arbitrary header content.
+
+    Since the zip format is defined relative to the end of a file, a zip file can have arbitrary
+    content pre-pended to it and not affect the validity of the zip archive. This class allows
+    identifying if a Zip has arbitrary header content and then isolating that content from the zip
+    archive.
+
+    N.B.: Zips that need Zip64 extensions are not supported yet.
+    """
+
     @classmethod
     def load(cls, path):
         # type: (str) -> Zip
-        return cls(
-            end_of_central_directory_record=_EndOfCentralDirectoryRecord.load(path), path=path
-        )
+        """Loads a zip file with detection of header presence.
 
-    _end_of_central_directory_record = attr.ib()  # type: _EndOfCentralDirectoryRecord
-    path = attr.ib()  # type: str
-    header_size = attr.ib(init=False)  # type: int
-
-    @header_size.default
-    def _header_size(self):
-        start_of_zip_offset_from_eof = (
-            self._end_of_central_directory_record.size
-            + self._end_of_central_directory_record.cd_size
-            + self._end_of_central_directory_record.cd_offset
-        )
-        return max(
-            (os.path.getsize(self.path) - start_of_zip_offset_from_eof),
-            min(
-                fhr.relative_offset_of_local_header
-                for fhr in self._end_of_central_directory_record.file_header_records
+        :raises: :class:`ZipError` if the zip could not be analyzed for the presence of a header.
+        """
+        try:
+            eocd = _EndOfCentralDirectoryRecord.load(path)
+        except _Zip64Error as e:
+            raise attr.evolve(
+                e, message="The zip at {path} requires Zip64 support.".format(path=path)
             )
-            if self._end_of_central_directory_record.file_header_records
-            else 0,
-        )
+        header_size = os.path.getsize(path) - eocd.start_of_zip_offset_from_eof
+        return cls(path=path, header_size=header_size)
+
+    path = attr.ib()  # type: str
+    header_size = attr.ib()  # type: int
 
     @property
     def has_header(self):
         # type: () -> bool
+        """Returns `True` if this zip has arbitrary header content."""
         return self.header_size > 0
 
     def isolate_header(
@@ -217,25 +191,45 @@ class Zip(object):
         stop_at=None,  # type: Optional[bytes]
     ):
         # type: (...) -> bytes
+        """Writes any non-zip header content to the given output stream.
+
+        If `stop_at` is specified, all the header content up to the right-most (last) occurrence of
+        the `stop_at` byte pattern is encountered. If the `stop_at` byte pattern is found, it and
+        all the content after it and up until the start of the zip archive is returned.
+        """
+
         if not self.has_header:
             return b""
 
         remaining = self.header_size
         with open(self.path, "rb") as in_fp:
             if stop_at:
+                # Assume the `stop_at` pattern is closer to the end of the header content and search
+                # backwards from there to be more efficient. This supports the pattern of
+                # sandwiching "small" content between a head-based format (like Microsoft's PE
+                # format, Apple's Mach-O format, ELF and even PNG) and a tail-based format like zip.
+                #
+                # In practice, Windows console scripts are implemented as a single file with a PE
+                # loader executable head sandwiching a shebang line between it and a zip archive
+                # trailer. The loader uses knowledge of its own format and the zip format to find
+                # the sandwiched shebang line and then interpret it to find a suitable Python and
+                # then execute that Python interpreter against the file which Python sees as a zip
+                # with an embedded `__main__.py` entry point.
                 in_fp.seek(self.header_size, os.SEEK_SET)
                 while remaining > 0:
-                    in_fp.seek(-min(remaining, io.DEFAULT_BUFFER_SIZE), os.SEEK_CUR)
-                    chunk = in_fp.read(min(remaining, io.DEFAULT_BUFFER_SIZE))
-                    offset = chunk.rfind(stop_at)
+                    chunk_size = min(remaining, io.DEFAULT_BUFFER_SIZE)
+                    in_fp.seek(-chunk_size, os.SEEK_CUR)
+                    chunk = in_fp.read(chunk_size)
                     remaining -= len(chunk)
+
+                    offset = chunk.rfind(stop_at)
                     if offset != -1:
                         remaining += offset
                         break
 
             excess = self.header_size - remaining
             in_fp.seek(0, os.SEEK_SET)
-            for chunk in iter(lambda: in_fp.read(min(io.DEFAULT_BUFFER_SIZE, remaining)), b""):
+            for chunk in iter(lambda: in_fp.read(min(remaining, io.DEFAULT_BUFFER_SIZE)), b""):
                 remaining -= len(chunk)
                 out_fp.write(chunk)
 
@@ -243,9 +237,8 @@ class Zip(object):
 
     def isolate_zip(self, out_fp):
         # type: (BinaryIO) -> None
-        if not self.has_header:
-            return
-
+        """Writes the pure zip archive portion of this zip file to the given output stream."""
         with open(self.path, "rb") as in_fp:
-            in_fp.seek(self.header_size, os.SEEK_SET)
+            if self.has_header:
+                in_fp.seek(self.header_size, os.SEEK_SET)
             shutil.copyfileobj(in_fp, out_fp)
